88007488503d7b5c2961cad8a5a5510a3f803567
Eli Hayon <dyslexicanaboko@gmail.com>
2024-05-17 23:04:18 -0400

    Fixed the dapper and static sql templates, they were not generating the DELETE statement correctly. Found another small mistake in the Static template where there was doubled-up quotations for some reason on a parameter.

--- SpotWelder.Lib/Templates/RepositoryDapper.cs
+++ SpotWelder.Lib/Templates/RepositoryDapper.cs
@@ -8,47 +8,47 @@ using System.Linq;
 
 namespace {{Namespace}}
 {
-    public class {{ClassName}}Repository
-    {
+		public class {{ClassName}}Repository
+		{
 		public {{EntityName}} Select({{PrimaryKeyType}} {{PrimaryKeyParameter}})
 		{
-      const string sql = @"
+			const string sql = @"
 			SELECT
 {{SelectAllList}}
 			FROM {{Schema}}.{{Table}}
 			WHERE {{PrimaryKeyColumn}} = @{{PrimaryKeyProperty}}";
 
-      using var connection = new SqlConnection(ConnectionString);
-      
-      var lst = connection.Query<{{EntityName}}>(sql, new { {{PrimaryKeyProperty}} = {{PrimaryKeyParameter}}}).ToList();
-        
-      return !lst.Any()? null : lst.Single();
+			using var connection = new SqlConnection(ConnectionString);
+			
+			var lst = connection.Query<{{EntityName}}>(sql, new { {{PrimaryKeyProperty}} = {{PrimaryKeyParameter}}}).ToList();
+				
+			return !lst.Any()? null : lst.Single();
 		}
 
 		public IEnumerable<{{EntityName}}> SelectAll()
 		{
-      const string sql = @"
+			const string sql = @"
 			SELECT
 {{SelectAllList}}
 			FROM {{Schema}}.{{Table}}";
 
-      using var connection = new SqlConnection(ConnectionString);
-      
-      return connection.Query<{{EntityName}}>(sql).ToList();
+			using var connection = new SqlConnection(ConnectionString);
+			
+			return connection.Query<{{EntityName}}>(sql).ToList();
 		}
 
 		//Preference on whether or not insert method returns a value is up to the user and the object being inserted
 		public {{PrimaryKeyType}} Insert({{EntityName}} entity)
 		{
-      const string sql = @"INSERT INTO {{Schema}}.{{Table}} (
+			const string sql = @"INSERT INTO {{Schema}}.{{Table}} (
 {{InsertColumnList}}
-            ) VALUES (
+						) VALUES (
 {{InsertValuesList}});
 {{ScopeIdentity}}";
 
-      using var connection = new SqlConnection(ConnectionString);
+			using var connection = new SqlConnection(ConnectionString);
 
-      var p = new DynamicParameters();
+			var p = new DynamicParameters();
 {{DynamicParametersInsert}}
 				
 {{PrimaryKeyInsertExecution}}
@@ -56,11 +56,11 @@ namespace {{Namespace}}
 
 		public void Update({{EntityName}} entity)
 		{
-      const string sql = @"UPDATE {{Schema}}.{{Table}} SET 
+			const string sql = @"UPDATE {{Schema}}.{{Table}} SET 
 {{UpdateParameters}}
-            WHERE {{PrimaryKeyColumn}} = @{{PrimaryKeyProperty}}";
+						WHERE {{PrimaryKeyColumn}} = @{{PrimaryKeyProperty}}";
 
-      using var connection = new SqlConnection(ConnectionString);
+			using var connection = new SqlConnection(ConnectionString);
 
 			var p = new DynamicParameters();
 {{DynamicParametersUpdate}}
@@ -68,16 +68,16 @@ namespace {{Namespace}}
 			connection.Execute(sql, p);
 		}
 
-    public void Delete({{EntityName}} entity)
-    {
-      const string sql = "DELETE FROM {{Schema}}.{{Table}} WHERE {{PrimaryKeyColumn}} = @{{PrimaryKeyProperty}}";
+		public void Delete({{EntityName}} entity)
+		{
+			const string sql = "DELETE FROM {{Schema}}.{{Table}} WHERE {{PrimaryKeyColumn}} = @{{PrimaryKeyProperty}}";
 
-      using var connection = new SqlConnection(ConnectionString);
+			using var connection = new SqlConnection(ConnectionString);
 
-      var p = new DynamicParameters();
-{{DynamicParametersUpdate}}
+			var p = new DynamicParameters(); 
+{{DynamicParametersDelete}}
 
-      connection.Execute(sql, p);
-    }
+			connection.Execute(sql, p);
+		}
 	}
 }
