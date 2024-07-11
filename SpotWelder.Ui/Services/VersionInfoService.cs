296640bed23ece793360ad550d30d26e2b9956fc
Eli Hayon <dyslexicanaboko@gmail.com>
2024-07-10 23:14:08 -0400

    #5 Removing the old WinForms based about window and creating my own in WPF. It actually displays the data I need now and I am going to add more to it later.

---
+++ SpotWelder.Ui/Services/VersionInfoService.cs
@@ -0,0 +1,57 @@
+﻿using SpotWelder.Ui.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SpotWelder.Ui.Services
+{
+  public class VersionInfoService
+  {
+    private readonly Assembly _assembly;
+
+    public ObservableCollection<VersionInfoViewModel> Properties { get; set; } = new ();
+
+    public const string Website = "http://obscureproblemsandgotchas.com/dyslexicapps";
+    public const string GitHub = "https://github.com/dyslexicanaboko/spot-welder";
+
+    public VersionInfoService()
+    {
+       _assembly = Assembly.GetExecutingAssembly();
+    }
+
+    public void LoadVersionInfo()
+    {
+      Add("Product", GetAssemblyAttribute<AssemblyProductAttribute>("Product"));
+      Add("Version", _assembly.GetName().Version.ToString());
+      Add("Company", GetAssemblyAttribute<AssemblyCompanyAttribute>("Company"));
+      Add("Copyright", GetAssemblyAttribute<AssemblyCopyrightAttribute>("Copyright"));
+      Add("Assembly", GetAssemblyTitleAttribute());
+      Add("Description", GetAssemblyAttribute<AssemblyDescriptionAttribute>("Description"));
+    }
+
+    private void Add(string key, string value)
+      => Properties.Add(new VersionInfoViewModel(key, value));
+
+    private string GetAssemblyTitleAttribute()
+    {
+      var title = GetAssemblyAttribute<AssemblyTitleAttribute>("Title");
+
+      return title != string.Empty ? 
+        title : 
+        Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+    }
+
+    private string GetAssemblyAttribute<T>(string propertyName)
+    {
+      var attributes = _assembly.GetCustomAttributes(typeof(T), false);
+
+      if (!attributes.Any()) return string.Empty;
+
+      var t = ((T)attributes.First());
+
+      return Convert.ToString(t!.GetType().GetProperty(propertyName)?.GetValue(t)) ?? string.Empty;
+    }
+  }
+}
---
+++ SpotWelder.Ui/ViewModels/VersionInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SpotWelder.Ui.ViewModels
+{
+  public class VersionInfoViewModel
+    : ObservableObject
+  {
+    public VersionInfoViewModel(string key, string value)
+    {
+      Key = key;
+      Value = value;
+    }
+
+    public string Key { get; set; }
+    public string Value { get; set; }
+  }
+}
