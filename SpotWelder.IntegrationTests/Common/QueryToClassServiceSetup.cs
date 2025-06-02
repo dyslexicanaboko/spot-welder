0edc217f6f8af3157a35d8e56269f118ed7b98d2
Eli Hayon <dyslexicanaboko@gmail.com>
2025-06-01 22:45:32 -0400

    Wrote stub of larger integration test to have full program coverage to ensure that all generators are working as expected every time.

---
+++ SpotWelder.IntegrationTests/Common/QueryToClassServiceSetup.cs
@@ -0,0 +1,100 @@
+﻿using SpotWelder.Lib;
+using SpotWelder.Lib.DataAccess;
+using SpotWelder.Lib.Models;
+using SpotWelder.Lib.Services;
+using SpotWelder.Lib.Services.CodeFactory;
+using SpotWelder.Lib.Services.Generators;
+using SpotWelder.Lib.Services.TableQueryFormats;
+using System;
+
+namespace SpotWelder.IntegrationTests.Common
+{
+  public class QueryToClassServiceSetup
+  {
+    //Language, elections, server connection - these will probably have to be passed in later
+    public QueryToClassParameters GetParameters(SqlEngine sqlEngine)
+    {
+      var sp = BuildParameters(sqlEngine);
+
+      var p = new QueryToClassParameters();
+
+      p.LanguageType = CodeType.CSharp;
+      p.OverwriteExistingFiles = true;
+      p.Namespace = "NoOneCares";
+      p.SubjectName = "DataTypeTest";
+      p.EntityName = "DataTypeTestEntity";
+      p.ModelName = "DataTypeTestModel";
+      p.ServerConnection.SqlEngine = sqlEngine;
+      p.ServerConnection.ConnectionString = sp.ConnectionString;
+      p.ServerConnection.SourceSqlType = SourceSqlType.TableName;
+      p.ServerConnection.SourceSqlText = sp.FullTableName;
+      p.ServerConnection.TableQuery = sp.NameFormatService.ParseTableName(p.ServerConnection.SourceSqlText);
+
+      return p;
+    }
+
+    private SqlEngineTestParameters BuildParameters(SqlEngine sqlEngine)
+    {
+      ITableQueryFormatStrategy svcNameFormat;
+      string connectionString;
+      string fullTableName;
+      string sqlQuery;
+
+      switch (sqlEngine)
+      {
+        case SqlEngine.SqlServer:
+          svcNameFormat = new SqlServerTableQueryFormatStrategy();
+          connectionString = "Server=.;Database=SpotWelder;Integrated Security=SSPI;Encrypt=False;";
+          fullTableName = "dbo.DataTypeTest";
+          sqlQuery = "SELECT * FROM dbo.DataTypeTest"; //Will have to update this later to include the columns
+          break;
+        case SqlEngine.Postgres:
+          svcNameFormat = new PostgresTableQueryFormatStrategy();
+          connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=spotwelder";
+          fullTableName = "public.data-type-test";
+          sqlQuery = "SELECT * FROM public.data-type-test";
+          break;
+        default:
+          throw new NotImplementedException($"SQL engine {sqlEngine} is not supported yet. 0x202506012228");
+      }
+
+      return new SqlEngineTestParameters
+      {
+        SqlEngine = sqlEngine,
+        NameFormatService = svcNameFormat,
+        ConnectionString = connectionString,
+        FullTableName = fullTableName,
+        SqlQuery = sqlQuery
+      };
+    }
+
+    public IQueryToClassService GetQueryToClassService(QueryToClassParameters parameters)
+    {
+      //For each Election put the corresponding generator in the array
+
+
+      return new QueryToClassService(
+        new QueryToClassRepository(),
+        new GeneralDatabaseQueries(),
+        new CodeGenerationFactory(new GeneratorBase[] {
+          //Array needs to be dynamic - whatever is chosen for the elections needs the corresponding generator
+          new ClassEntityGenerator(),
+          new MapperGenerator()
+        }));
+    }
+  }
+
+  public class SqlEngineTestParameters
+  {
+    public SqlEngine SqlEngine { get; set; }
+
+    public ITableQueryFormatStrategy NameFormatService { get; set; }
+
+    public string ConnectionString { get; set; }
+    
+    public string FullTableName { get; set; }
+    
+    public string SqlQuery { get; set; }
+  }
+
+}
---
+++ SpotWelder.IntegrationTests/ModelGenerationTests/ModelParityTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using SpotWelder.IntegrationTests.Common;
+using SpotWelder.Lib;
+using SpotWelder.Lib.DataAccess;
+using SpotWelder.Lib.Models;
+using SpotWelder.Lib.Services;
+using SpotWelder.Lib.Services.CodeFactory;
+using SpotWelder.Lib.Services.Generators;
+using SpotWelder.Lib.Services.TableQueryFormats;
+
+namespace SpotWelder.IntegrationTests;
+
+[TestFixture]
+public class ModelParityTests 
+  : SpotWelderTestBase
+{
+
+  private QueryToClassServiceSetup _svc;
+
+  public void Setup()
+  {
+    _svc = new QueryToClassServiceSetup();  
+  }
+
+  [Test]
+  public void ModelParity_ClassEntity(SqlEngine sqlEngine)
+  {
+    //Get parameters for the SQL engine
+    var p = new QueryToClassServiceSetup().GetParameters(sqlEngine);
+
+    //Iterate through the elections one at a time
+    p.Elections = GenerationElections.GenerateEntity;
+
+    //Set the corresponding generator per election
+    var svc = _svc.GetQueryToClassService(p);
+
+    //Generate the class entity
+    var lst = svc.Generate(p);
+
+    //Perform comparisons on the expected output versus the generated output
+    Assert.That(lst, Is.Not.Null);
+    Assert.That(lst.Count, Is.GreaterThan(0));
+  }
+}
